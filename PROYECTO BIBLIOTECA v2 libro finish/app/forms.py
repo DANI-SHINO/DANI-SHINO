9b54817c791be4918cc0f41629ed38a1e7d8830e
DANIEL <147718556+DANI-SHINO@users.noreply.github.com>
2025-06-20 01:15:54 -0500

    Add files via upload

---
+++ PROYECTO BIBLIOTECA v2 libro finish/app/forms.py
@@ -0,0 +1,96 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SelectField, DateField, SubmitField, TextAreaField, IntegerField
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp, ValidationError, Optional, NumberRange
+from app.models import Usuario
+from datetime import date
+
+class RegistroForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired(), Length(min=4, max=25)])
+    password = PasswordField('Contraseña', validators=[
+        DataRequired(),
+        Length(min=8, message='La contraseña debe tener al menos 8 caracteres'),
+        Regexp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$', message='La contraseña debe ser alfanumérica (letras y números).')
+    ])
+    confirmar_password = PasswordField('Confirmar contraseña', validators=[
+        DataRequired(),
+        EqualTo('password', message='Las contraseñas deben coincidir')
+    ])
+    submit = SubmitField('Registrarse')
+
+    def validate_username(self, username):
+        usuario = Usuario.query.filter_by(username=username.data).first()
+        if usuario:
+            raise ValidationError('El nombre de usuario ya está en uso. Por favor elige otro.')
+
+class LibroForm(FlaskForm):
+    isbn = StringField('ISBN', validators=[DataRequired()])
+    titulo = StringField('Título', validators=[DataRequired()])
+    autor = StringField('Autor', validators=[DataRequired()])
+    descripcion = TextAreaField('Descripción')
+    categoria = SelectField(
+        'Categoría',
+        choices=[
+            ('novela', 'Novela'),
+            ('filosofia', 'Filosofía'),
+            ('poesia', 'Poesía'),
+            ('teatro', 'Teatro'),
+            ('ensayo', 'Ensayo'),
+            ('cronica', 'Crónica'),
+            ('historieta', 'Historieta'),
+            ('biografia', 'Biografía'),
+            ('cuento', 'Cuento'),
+            ('audiolibro', 'Audiolibro')
+        ],
+        validators=[DataRequired()]
+    )
+    editorial = StringField('Editorial')
+    fecha_publicacion = DateField(
+        'Fecha de Publicación',
+        format='%Y-%m-%d',
+        validators=[Optional()]
+    )
+    cantidad_total = IntegerField(
+        'Cantidad Total',
+        validators=[DataRequired(), NumberRange(min=0)]
+    )
+    submit = SubmitField('Guardar')
+
+class EditarLibroForm(FlaskForm):
+    titulo = StringField("Título", validators=[DataRequired()])
+    autor = StringField("Autor", validators=[DataRequired()])
+    descripcion = TextAreaField("Descripción", validators=[Optional()])
+    categoria = SelectField(
+        "Categoría",
+        choices=[
+            ('novela', 'Novela'),
+            ('filosofia', 'Filosofía'),
+            ('poesia', 'Poesía'),
+            ('teatro', 'Teatro'),
+            ('ensayo', 'Ensayo'),
+            ('cronica', 'Crónica'),
+            ('historieta', 'Historieta'),
+            ('biografia', 'Biografía'),
+            ('cuento', 'Cuento'),
+            ('audiolibro', 'Audiolibro')
+        ],
+        validators=[DataRequired()]
+    )
+    cantidad_total = IntegerField(
+        "Cantidad Total",
+        validators=[Optional(), NumberRange(min=0)]
+    )
+    cantidad_disponible = IntegerField(
+        "Cantidad Disponible",
+        validators=[Optional(), NumberRange(min=0)]
+    )
+    fecha_publicacion = DateField(
+        "Fecha de Publicación",
+        format='%Y-%m-%d',
+        validators=[Optional()]
+    )
+    editorial = StringField(
+        "Editorial",
+        validators=[Optional()]
+    )
+    submit = SubmitField("Guardar cambios")
+
